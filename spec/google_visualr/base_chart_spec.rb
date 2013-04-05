f38dc9dbf14a1385e22113ea5bb48e4e5a4735c0
Craig Morrissey <craig@proactivelygreen.com>
2013-04-05 07:59:42 -0400

    add specs

--- spec/google_visualr/base_chart_spec.rb
+++ spec/google_visualr/base_chart_spec.rb
@@ -40,6 +40,13 @@ describe GoogleVisualr::BaseChart do
       js = @chart.to_js("body")
       js.should == base_chart_with_listener_js("body")
     end
+
+    it "generates JS with listeners that use jquery proxy" do
+      @chart.add_listener("select", "function() {test_event(chart);}")
+
+      js = @chart.to_js("body", :use_jquery_proxy => true)
+      js.should == base_chart_with_listener_js_with_jquery_proxy("body")
+    end
   end
 
 end
--- spec/support/common.rb
+++ spec/support/common.rb
@@ -40,3 +40,15 @@ def base_chart_with_listener_js(div_class="div_class")
 </script>
 JS
 end
+
+def base_chart_with_listener_js_with_jquery_proxy(div_class="div_class")
+  js  = <<JS
+<script type='text/javascript'>
+  google.load('visualization','1', {packages: ['basechart'], callback: function() {
+    var data_table = new google.visualization.DataTable();data_table.addColumn({\"type\":\"string\",\"label\":\"Year\"});data_table.addColumn({\"type\":\"number\",\"label\":\"Sales\"});data_table.addColumn({\"type\":\"number\",\"label\":\"Expenses\"});data_table.addRow([{v: \"2004\"}, {v: 1000}, {v: 400}]);data_table.addRow([{v: \"2005\"}, {v: 1200}, {v: 450}]);data_table.addRow([{v: \"2006\"}, {v: 1500}, {v: 600}]);data_table.addRow([{v: \"2007\"}, {v: 800}, {v: 500}]);\n    var chart = new google.visualization.BaseChart(document.getElementById('#{div_class}'));
+    google.visualization.events.addListener(chart, 'select', jQuery.proxy(function() {test_event(chart);}, { chart: chart, data_table: data_table }));
+    chart.draw(data_table, {legend: \"Test Chart\", width: 800, is3D: true});
+  }});
+</script>
+JS
+end
